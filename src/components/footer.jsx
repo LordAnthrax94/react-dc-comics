9cc4a5f886b010175ef4773b09d42b42bd66fd27
LordAnthrax94 <andreagi1994@gmail.com>
2025-01-13 18:58:03 +0100

    fix: compilato footer e main(da finire)

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,70 @@
 const Footer = () =>{
   return (
-    <footer>Footer</footer>
+    <footer>
+     <div className="container d-flex justify-content-between">
+
+      <div className="listeFinali d-flex mt-5">
+          
+        <div className="firstNav navStyle">
+          <ul><span>DC COMICS</span>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">News</a></li>
+          </ul>
+          <ul><span>SHOP</span>
+            <li><a href="#">Shop DC</a></li>
+            <li><a href="#">Shop DC Collectables</a></li>            
+          </ul>
+
+        </div>
+
+        <div className="secondNav navStyle">
+          <ul><span>DC</span>
+            <li><a href="#">Terms Of Use</a></li>
+            <li><a href="#">Privacy Policy(New)</a></li>
+            <li><a href="#">Ad Choice</a></li>
+            <li><a href="#">Advertising</a></li>
+            <li><a href="#">Jobs</a></li>
+            <li><a href="#">Subscription</a></li>
+            <li><a href="#">Talent Workshop</a></li>
+            <li><a href="#">CPSC Certificates</a></li>
+            <li><a href="#">Ratings</a></li>
+            <li><a href="#">Shop Help</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
+        </div>
+
+        <div className="thirdNav navStyle">
+          <ul><span>SITES</span>
+            <li><a href="#">DC</a></li>
+            <li><a href="#">MAD Magazines</a></li>
+            <li><a href="#">DC Kids</a></li>
+            <li><a href="#">DC Universe</a></li>
+            <li><a href="#">DC Power Visa</a></li>
+
+          </ul>
+
+        </div>
+      </div>
+
+      <div className="logoFooter">
+        <img src="../Public/img/dc-logo-bg.png" alt="LogoBg" />
+      </div>
+
+
+     </div>
+      <div className="bg-dark">
+        <div className="container">
+        
+        </div>
+      </div>
+        
+
+    </footer>
   )
 }
 
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,17 +2,24 @@ const Main = () =>{
   return (
     <main>
       <div className="cardContent">
-        <div className="container">
-          <p>Content goes here </p>
+        <div className="container d-flex">
+          <p className="align-items-center">Content goes here</p>
         </div>
         
       </div>
-      <div className ="linkContent">
-        <div className="container">
-
+      <div className ="linkContent bg-primary">
+        <div className="listNav container d-flex justify-content-between align-items-center">
+          <div className="d-flex align-items-center">
+            <img src="../public/img/buy-comics-digital-comics.png" alt="buyLogo" />
+            <a className="nav-link" href="#">DIGITAL COMICS</a>
+          </div>           
+           
+           <a className="nav-link" href="#">Features</a>
+           <a className="nav-link" href="#">Pricing</a>
+           <a className="nav-link" href="#">Disabled</a>
+           <a className="nav-link" href="#">Disabled</a>
+          </div>
         </div>
-      </div>
-      
     </main>
   )
 }
